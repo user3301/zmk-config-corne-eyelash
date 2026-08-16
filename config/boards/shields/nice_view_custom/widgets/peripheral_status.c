a8ff15d6ea3a46bea464aab7636f41e0c574582f
user3301 <26126682+user3301@users.noreply.github.com>
2026-08-16 17:18:42 +1000

    Scale up nice_view custom logo art by 1.25x

--- config/boards/shields/nice_view_custom/widgets/peripheral_status.c
+++ config/boards/shields/nice_view_custom/widgets/peripheral_status.c
@@ -115,6 +115,8 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
 
     lv_obj_t *art = lv_img_create(widget->obj);
     lv_img_set_src(art, &images);
+    lv_img_set_pivot(art, 0, 0);
+    lv_img_set_zoom(art, 320);
     lv_obj_align(art, LV_ALIGN_TOP_LEFT, 0, 0);
 
     sys_slist_append(&widgets, &widget->node);
